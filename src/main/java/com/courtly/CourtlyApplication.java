7ed9592191f47488bdc0f7bfaf5cf939656a8a1b
vladimirtischler <56106321+vladimirtischler@users.noreply.github.com>
2025-08-02 11:58:32 +0200

    init commit

---
+++ src/main/java/com/courtly/CourtlyApplication.java
@@ -0,0 +1,13 @@
+package com.courtly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourtlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourtlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/courtly/CourtlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.courtly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourtlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
